541da50b4e6884d8b770e3ca630d453d1ea71d04
user <gauthamivasanth92@gmail.com>
2023-10-13 15:16:42 +0000

    Added UserRepository Model

---
+++ src/main/java/com/example/restdemo/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.restdemo.repository;
+
+import com.example.restdemo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Integer>{
+
+}
