29a44a05841d01d25125122605ab6d8a3470b614
Meghana <kmmeghanakote@gmail.com>
2023-10-18 11:20:00 +0000

    changed name of controllerTest to controller

--- src/test/java/com/example/restdemo/controller/DoctorControllerTest.java
+++ src/test/java/com/example/restdemo/controller/DoctorControllerTest.java
@@ -1,7 +1,6 @@
-package com.example.restdemo.controllerTest;
-
-import com.example.restdemo.model.Patient;
-import com.example.restdemo.repository.PatientRepository;
+package com.example.restdemo.controller;
+import com.example.restdemo.model.Doctor;
+import com.example.restdemo.repository.DoctorRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,57 +8,43 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-
 import java.util.List;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class PatientControllerTest {
+public class DoctorControllerTest {
 
-    // MockMvc provides a way to test Spring MVC controllers by sending HTTP requests into the DispatcherServlet
-    // and asserting the response details.
     @Autowired
     private MockMvc mockMvc;
 
-    // ObjectMapper provides functionality to convert between Java objects and their JSON representation.
     @Autowired
     private ObjectMapper objectMapper;
 
     // Setting up the expected list of Person objects which the controller should return.
-    List<Patient> expectedPatient = List.of(
-            new Patient(1L,"Smith","K",10,"10-24-1993","male","95641234562","smith.a@gmail.com","Dublin"),
-            new Patient(2L,"Raj","M",10,"10-24-1990","male","95641234552","raj.a@gmail.com","Dublin")
+    List<Doctor> expectedDoctor = List.of(
+            new Doctor(1L, "Jane", 45454464L,"jane@gmail.com","123 Main St","Oncology"),
+            new Doctor(2L,"Mark",55578544L,"mark@gmail.com","456 Main St","Chemo"),
+            new Doctor(3L,"Sam",454545454L,"sam@gmail.com","798 Main St","ICU"));
 
-    );
-
-    // PersonRepository - we will use it
-    //  to save Person objects
-    //  from the expectedPeople List
-    //  to DB
-    //  before calling the endpoint
     @Autowired
-    private PatientRepository patientRepository;
+    private DoctorRepository doctorRepository;
 
-    // Before each test method is run, this setup method will be invoked.
-    // As of now, it's empty, but you can add any initial setup for your tests here.
     @BeforeEach
     public void setup() {}
 
-    // The Test annotation indicates that this method is a test method.
     @Test
-    public void testGetAllPatient() throws Exception {
+    public void testGetAllPeople() throws Exception {
         // Save the objects to DB
-        patientRepository.saveAll(expectedPatient);
+        doctorRepository.saveAll(expectedDoctor);
 
         // Converting the expected list of people to a JSON string.
-        String expectedJsonResponse = objectMapper.writeValueAsString(expectedPatient);
+        String expectedJsonResponse = objectMapper.writeValueAsString(expectedDoctor);
 
         // Using mockMvc to perform an HTTP GET request to /people
-        mockMvc.perform(get("/patient"))
+        mockMvc.perform(get("/doctor"))
                 // Expecting the HTTP status to be 200 OK
                 .andExpect(status().isOk())
                 // Expecting the response body to be the JSON representation of the expected list of people.
