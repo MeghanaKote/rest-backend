bed4e903d8b785a3118d57254e7cd8759554eb5b
Dalimol <dalimol.a@gmail.com>
2023-10-18 11:23:04 +0000

    Added Patient controller change

--- src/main/java/com/example/restdemo/controller/PatientController.java
+++ src/main/java/com/example/restdemo/controller/PatientController.java
@@ -3,7 +3,10 @@ package com.example.restdemo.controller;
 import com.example.restdemo.model.Patient;
 import com.example.restdemo.repository.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
