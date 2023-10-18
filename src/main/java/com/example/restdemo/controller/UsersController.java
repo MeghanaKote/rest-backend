731ef8db6d1c578dd8799e73bba74ef2ffdc8e23
user <gauthamivasanth92@gmail.com>
2023-10-18 12:01:36 +0000

    Users Name changes and test class

--- src/main/java/com/example/restdemo/controller/UsersController.java
+++ src/main/java/com/example/restdemo/controller/UsersController.java
@@ -1,7 +1,7 @@
 package com.example.restdemo.controller;
 
-import com.example.restdemo.model.User;
-import com.example.restdemo.repository.UserRepository;
+import com.example.restdemo.model.Users;
+import com.example.restdemo.repository.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,12 +10,12 @@ import java.util.List;
 
 @RestController
 
-public class UserController {
+public class UsersController {
     @Autowired
-    private UserRepository userRepository;
+    private UsersRepository usersRepository;
     @GetMapping("/user")
-    public List<User> getAllUsers(){
-        return userRepository.findAll();
+    public List<Users> getAllUsers(){
+        return usersRepository.findAll();
 
 
     }
